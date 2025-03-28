8db54023f401bcb2546c5ce977c245328b190b78
Haykuhi Mkrtchyan <picsartacademy@HMs-MacBook.local>
2025-03-28 14:34:58 +0400

    added URL

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = `http://localhost:${import.meta.env?.VITE_BACKEND_PORT || 5001}`; 
+const API_BASE_URL = `${import.meta.env?.API_URL}:${import.meta.env?.VITE_BACKEND_PORT || 5001}`; 
 
 console.log("API_BASE_URL:", API_BASE_URL);  
 
