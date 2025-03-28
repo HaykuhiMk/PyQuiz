b58f916428deae7815df10f77b94dabdd13a3803
Haykuhi Mkrtchyan <picsartacademy@HMs-MacBook.local>
2025-03-28 12:56:16 +0400

    Final Version

--- frontend/public/js/load_header_footer.js
+++ frontend/public/js/load_header_footer.js
@@ -1,11 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
+            const burger = document.querySelector(".burger-menu");
+            const navMenu = document.querySelector("nav ul");
+
+            if (burger && navMenu) {
+                burger.addEventListener("click", function () {
+                    navMenu.classList.toggle("active");
+                });
+            }
         })
         .catch(error => console.error('Error loading header:', error));
-
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
