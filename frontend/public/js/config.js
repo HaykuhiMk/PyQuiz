a8b3c06d1afb7ccaad4ae3e2c200f3e6ccf13508
johnasatryan <john.asatryan@gmail.com>
2025-03-29 12:20:34 +0400

    fixed

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -1,5 +1,2 @@
-const API_BASE_URL = `${import.meta.env?.API_URL}:${import.meta.env?.VITE_BACKEND_PORT || 5001}`; 
-
-console.log("API_BASE_URL:", API_BASE_URL);  
-
+const API_BASE_URL = 'localhost:5001';
 export default API_BASE_URL;
