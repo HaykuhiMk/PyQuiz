419fcd014c19ad9b50123b7b55ff24c27a4a6290
john-asatryan <john.asatryan@picsart.academy>
2025-03-29 11:22:05 +0400

    add serving

---
+++ frontend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve JS and CSS from the public folder
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+
+// Serve HTML files from the views folder
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
